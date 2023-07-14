b36cdc8557ddff1cf59faf470bdea2685076ddea
gaechkahub <keozon@gmail.com>
2023-07-14 06:46:23 +0200

    4,5

--- 3-UsingAPIs/Week2/homework/ex4-diceRace.js
+++ 3-UsingAPIs/Week2/homework/ex4-diceRace.js
@@ -15,18 +15,25 @@ const rollDie = require('../../helpers/pokerDiceRoller');
 
 function rollDice() {
   const dice = [1, 2, 3, 4, 5];
-  // TODO complete this function; use Promise.race() and rollDie()
+  const arrayOfPromises = dice.map(rollDie);
+  return Promise.race(arrayOfPromises);
 }
 
 // Refactor this function to use async/await and try/catch
-function main() {
-  rollDice()
-    .then((results) => console.log('Resolved!', results))
-    .catch((error) => console.log('Rejected!', error.message));
+async function main() {
+  try {
+    const result = await rollDice();
+    console.log('Resolved!', result);
+  } catch (error) {
+    console.log('Rejected!', error.message);
+  }
 }
-
 // ! Do not change or remove the code below
 if (process.env.NODE_ENV !== 'test') {
   main();
 }
 module.exports = rollDice;
+
+// The code inside the rollOnce() function still continues
+// to execute because there is no conditional check to stop
+// the execution after a rejection.
